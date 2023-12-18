798469b051573fd7eb2f5a40af36b52a394f18bb
akinori <aki10.working@gmail.com>
2023-12-18 15:32:19 +0900

    fix

--- page.php
+++ page.php
@@ -4,8 +4,10 @@
     <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
         <div class="p-6 mb-8 c-container">
           <div class="p-page-fv c-fade-in">
+            <?php if(has_post_thumbnail()): ?>
             <img class="hiddem md:block absolute w-full h-full" src="<?= get_the_post_thumbnail_url(); ?>" alt="">
             <img class="md:hidden absolute w-full h-full" src="<?php the_field('sp_image') ; ?>" alt="">
+            <?php endif; ?>
             <div class="sm:w-2/3 z-10">
               <h1 class="p-page-fv__title"><?php the_title(); ?></h1>
               <div><?php the_field('en_title'); ?></div>
